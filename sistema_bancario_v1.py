34faa97966d38c117549aa3086265229a645f8bf
johnstack <joaovictormtl@gmail.com>
2024-05-16 16:47:01 -0300

    refactor: melhora na estrutura do código em sistema_bancario_v1.py

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -1,11 +1,9 @@
-opcao = -1
 saldo = 800
 saques_diarios = 0
-saques = 0
-depositos = 0
+extrato = ""
 limite = 500
 
-while opcao != 0:
+while True:
     print("""
 [1] Sacar
 [2] Depositar
@@ -16,12 +14,12 @@ while opcao != 0:
 
     if opcao == 1:
         if saques_diarios < 3:
-            valor = float(input("Valor de saque: "))
+            valor = float(input("\nValor de saque: "))
 
             if saldo >= valor:
                 if valor <= limite:
                     saldo -= valor
-                    saques += valor
+                    extrato += f"Saque: R$ {valor:.2f}\n"
                     saques_diarios += 1
                 else:
                     print("Saque superior ao limite permitido")
@@ -30,16 +28,19 @@ while opcao != 0:
         else:
             print("Limite de Saques diários alcançado.")
     elif opcao == 2:
-        valor = float(input("Valor de depósito: "))
+        valor = float(input("\nValor de depósito: "))
 
         if valor > 0:
             saldo += valor
-            depositos += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
         else:
             print("Deposite um valor válido!")
     elif opcao == 3:
+        print("\n===== EXTRATO =====")
+        print("Não foram realizadas movimentações.\n" if not extrato else extrato)
         print(f"Saldo: {saldo:.2f}")
-        print(f"Saques: {saques:.2f}")
-        print(f"Depósitos: {depositos:.2f}")
-else:
-    print("Saindo...")
+    elif opcao == 0:
+        print("Saindo...")
+        break
+    else:
+        print("Digite uma opcao válida!")
